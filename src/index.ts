14b3d4c85549e814045687ed7a3faa9da96a0c54
Konrad <kowcarz@o2.pl>
2022-10-12 12:37:09 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 function hello() {
     console.log("Witaj w swiecie Typescript.")
+    console.log("Witaj w swiecie Typescript2.")
+    console.log("Witaj w swiecie Typescript3.")
+    console.log("Witaj w swiecie Typescript4.")
 }
 
 hello();
