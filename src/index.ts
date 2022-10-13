1e90f7cd37ea87dea36139ae10cb01052370c760
Konrad_Owcarz <kowcarz@o2.pl>
2022-10-13 15:16:16 +0200

    Test2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ function hello() {
     console.log("Witaj w swiecie Typescript2.")
     console.log("Witaj w swiecie Typescript3.")
     console.log("Witaj w swiecie Typescript4.")
+    console.log("Test zmian");
 }
 
 hello();
